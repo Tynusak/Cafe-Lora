ef6b4c96e0ca34732e265e83315aff3888dcee19
Kristýna Švandová <kristyna.svandova@gmail.com>
2022-05-04 21:58:57 +0200

    Funkční objednávání

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './style.css';
 
-console.log('funguju!');
 document.querySelector('#nav-btn').addEventListener('click', () => {
   document.querySelector('nav').classList.toggle('nav-closed');
 });
@@ -11,3 +10,19 @@ for (let i = 0; i < hamburgerMenu.length; i++) {
     document.querySelector('nav').classList.add('nav-closed');
   });
 }
+let ordered = false;
+const orderButton = document.querySelector('.order-btn');
+
+orderButton.addEventListener('click', () => {
+  if (!ordered) {
+    orderButton.textContent = 'Zrušit';
+    document.querySelector('.drink__cup').classList.add('drink__cup--selected');
+    ordered = true;
+  } else {
+    orderButton.textContent = 'Objednat';
+    document
+      .querySelector('.drink__cup')
+      .classList.remove('drink__cup--selected');
+    ordered = false;
+  }
+});
