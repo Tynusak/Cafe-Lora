892c13e84a3ce4d5e7ea86f11562d49a7692211e
Kristýna Švandová <kristyna.svandova@gmail.com>
2022-05-04 13:46:28 +0200

    Funkční menu na úzké obrazovce

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './style.css';
 
 console.log('funguju!');
+document.querySelector('#nav-btn').addEventListener('click', () => {
+  document.querySelector('nav').classList.toggle('nav-closed');
+});
+
+const hamburgerMenu = document.querySelectorAll('a');
+for (let i = 0; i < hamburgerMenu.length; i++) {
+  hamburgerMenu[i].addEventListener('click', () => {
+    document.querySelector('nav').classList.add('nav-closed');
+  });
+}
